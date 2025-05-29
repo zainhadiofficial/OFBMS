c17c257833165133449022ffa4342d7a26aa69e1
Zain Hadi <zainhadiofficial@gmail.com>
2025-05-29 10:25:33 +0500

    Subviews

--- header.php
+++ header.php
@@ -6,42 +6,48 @@ session_start();
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300italic,300,400italic,600,600italic,700,700italic,800,800italic' rel='stylesheet' type='text/css'>
+    	<link href='//fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>	
+        <link href="https://fonts.googleapis.com/css2?family=Italianno&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-        <link href="https://fonts.googleapis.com/css2?family=Assistant:wght@200&display=swap" rel="stylesheet">
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
         <script src="https://kit.fontawesome.com/44f557ccce.js"></script>
-        <title>Online Flight Booking</title>          
-        <link rel = "icon" href =  
-            "../assets/images/brand.png" 
-        type = "image/x-icon">          
+        <!-- Bootstrap CSS -->
+		
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <title>Online Flight Booking</title>         
+        <link rel = "icon" href ="../assets/images/brand.png"  type = "image/x-icon">       
     </head>
 <style>
+.astext {
+    background:none;
+    border:none;
+    margin:0;
+    padding:0;
+    cursor: pointer;
+}    
 @font-face {
   font-family: 'product sans';
   src: url('../assets/css/Product Sans Bold.ttf');
 }
-button.btn-outline-light:hover {
-  color: cornflowerblue !important;
+h5:hover {
+    color: #E8E8E8;
 }
-  .navbar-custom {
-    /* background-color: #6495ED; */
-    background-color: #3a3a3a;
-    /* font-family: 'Bangers', cursive; */
-    font-family: 'product sans', cursive;    
-  }
-  h4 {
-    font-size: 23px !important;
-  }
-  
-.table .thead-dark th{
-  background-color: #316b83;
+h5 {
+    color: white;
+    font-weight: bold;
+    font-size: 20px ;
+	font-family: 'Montserrat', sans-serif;
+}
+.btn-login {
+    /* font-size: 22px ; */
+    font-weight: bold;
+	font-family: 'Montserrat', sans-serif;    
 }
 </style>
-    <body>
-
-        <nav class="navbar navbar-custom navbar-expand-lg navbar-dark bg-primary">
-          <a class="navbar-brand text-light" href="index.php"><h4>OFBMS - Admin</h4></a>
+    <body>        
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary ">
+        <a class="navbar-brand" href="index.php"><h5>OFBMS</h5>            
+        </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse"
             data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
             aria-expanded="false" aria-label="Toggle navigation">
@@ -50,76 +56,62 @@ button.btn-outline-light:hover {
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-          <?php
-              if(isset($_SESSION['adminId'])) { ?>
-                <ul class="navbar-nav mr-auto">
-
+            <ul class="navbar-nav mr-auto">     
                 <li class="nav-item">
                     <a class="nav-link" href="index.php">
-                      <h5 class="ml-2"> Dashboard</h5>
+                        <h5> Home</h5>
                     </a>
-                  </li>
-
-                  <li class="nav-item">
-                    <a class="nav-link" href="flight.php">
-                      <h5 class="ml-2"> Create Flight</h5>
+                </li>                           
+            <?php if(isset($_SESSION['userId'])) { ?>
+                <li class="nav-item">
+                    <a class="nav-link" href="my_flights.php">
+                        <h5> My Flights</h5>
                     </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="all_flights.php">
-                      <h5>Flights</h5>
+                </li>       
+                <li class="nav-item">
+                    <a class="nav-link" href="ticket.php">
+                        <h5> Tickets</h5>
                     </a>
-                  </li>   
-                  <li class="nav-item">
-                    <a class="nav-link" href="list_airlines.php">
-                      <h5>Airlines</h5>
-                    </a>
-                  </li>              
-                  <!-- <li class="nav-item">
-                    <a class="nav-link" href="review.php">
-                      <h4>Reviews</h4>
-                    </a>
-                  </li>                      -->
-                </ul>
-                <ul class="nav navbar-nav navbar-right">
-                  <li class="nav-item">
-                    <div class="dropdown mt-2">
-                      <button class="btn bg-transparent dropdown-toggle text-white" type="button" 
-                        id="dropdownMenuButton" data-toggle="dropdown" 
-                          aria-haspopup="true" aria-expanded="false">
-                        
-                        <i class="fa fa-plus text-white"></i> Airlines </td>
-                      </button>  
-                      <div class="dropdown-menu">
-                        <form class="px-2 py-2"  action="../includes/admin/airline.inc.php" method="post">
-                          <div class="form-group">
-                            <input type="text" class="form-control" name="airline" 
-                              placeholder="Airlines Name">
-                            <input type="number" class="form-control mt-3" name="seats" 
-                              placeholder="Total Seats">                              
-                          </div>  
-                          <button type="submit" name="air_but" 
-                            class="btn btn-success w-100">Submit</button>
-                        </form>
-                      </div>
-                    </div>  
-                  </li>
-                  <li class="nav-item  p-1 border-light ">
-                      <a class="nav-link" href="">
-                          <i class="ml-1 fa fa-user text-light"></i>
-                          <span class="nav_link text-light"
-                            style="font-size: 20px;">
-                          <?php echo  $_SESSION['adminUname']; ?>
-                          </span>
-                      </a>
-                  </li>            
-                </ul>                 
-                <form action="../includes/logout.inc.php" method="POST">
-                <button class="btn btn-outline-light m-2" type="submit">
-                    Logout </button>
-                </form> 
-            </div>
+                </li>                                                                  
             <?php } ?>
-
+                <li class="nav-item">
+                    <a class="nav-link" href="feedback.php">
+                        <h5> Feedback</h5>
+                    </a>
+                </li>               
+            </ul>           
+            <?php
+            if(isset($_SESSION['userId'])) {
+                echo '
+                <ul  class="nav navbar-nav navbar-right">
+                    <li class="nav-item mr-3">
+                            
+                            <h5 class=" text-light">
+                            <i class="ml-1 fa fa-user text-light"></i>
+                            '.$_SESSION['userUid'].'
+                            </h5>
+                    </li>          
+                    <li class="nav-item ml-3">
+                        <form action="includes/logout.inc.php" class="logout_form" method="POST">
+                        <button class="astext"  type="submit">
+                            <h5> Logout </h5>                            
+                            </button>
+                        </form> 
+                    </li>                       
+                </ul>    ';
+            } else {
+                echo '
+                <div class="dropdown "
+                    style="margin-right:70px">
+                <a class="text-light text-decoration-none dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <b>Login</b>                            
+                </a>
+                <div class="dropdown-menu w-75" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item " href="login.php">Passenger</a>
+                    <a class="dropdown-item " href="admin/login.php">Administrator</a>
+                </div>
+                </div>';
+            }
+            ?>
+        </div>
         </nav>
